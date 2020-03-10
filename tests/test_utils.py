9aea7b30724b741cdbc8c9529694de06c8dea1c3
Milk <zhengym28@mail2.sysu.edu.cn>
2020-03-10 18:30:29 +0800

    switch to pytest-datadir to fix path issue

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,11 @@
 from anndata import read_h5ad
-from pathlib import Path
 from spatialtis import CONFIG
 from spatialtis.utils import prepare_svca
-DATA_DIR = str(Path("tests/data/small.h5ad").absolute())
 
 CONFIG.EXP_OBS = ["Patient", "Part", "ROI"]
 CONFIG.CELL_TYPE_COL = "leiden"
 
-data = read_h5ad(DATA_DIR)
 
-
-def test_svca():
+def test_svca(shared_datadir):
+    data = read_h5ad(shared_datadir / 'small.h5ad')
     prepare_svca(data, "tmp/")
