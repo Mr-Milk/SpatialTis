6a084fb64e91684551d45c315ad969f78eb51605
Mr-Milk <zym.zym1220@gmail.com>
2020-09-04 16:55:05 +0800

    redraw logo using ai;fix readthedoc config

--- docs/source/api_index/plotting/palette.rst
+++ docs/source/api_index/plotting/palette.rst
@@ -2,4 +2,3 @@ Palettes functions
 ====================
 .. autofunction:: spatialtis.plotting.get_colors
 .. autofunction:: spatialtis.plotting.get_linear_colors
-.. autofunction:: spatialtis.plotting.view_palette
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,9 +63,9 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
-html_favicon = 'src/favicon-chrome.png'
+html_favicon = 'src/Logo.svg'
 
-html_logo = 'src/banner.png'
+html_logo = 'src/Logo-text.png'
 html_theme_options = {
     'logo_only': True,
 }
